aeb7f12345359cb04cf0207279574d9f6d01eeed
arturo castro <arturo@openframeworks.cc>
2017-12-06 12:57:26 +0100

    update build system

--- build.rs
+++ build.rs
@@ -2,26 +2,24 @@ use std::process::Command;
 use std::env;
 use std::fs;
 use std::path::Path;
-#[cfg(macos)]
 use std::ffi::OsString;
+use std::os::unix::ffi::OsStringExt;
 
-#[cfg(macos)]
-fn main() {
+fn build_macos() {
 	let freeimage_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 	let freeimage_native_dir = Path::new(&freeimage_dir).join("FreeImage");
-	let xcode_select_out = OsString::from_bytes(
-            Command::new("xcode-select")
+	let xcode_select_out: OsString = OsString::from_vec(Command::new("xcode-select")
                 .arg("-print-path")
                 .output().unwrap()
-		        .stdout
-        ).unwrap().into_string().unwrap();
-    let xcode_path = xcode_select_out.lines().next().unwrap().to_string();
+		        .stdout);
+    let xcode_path = xcode_select_out.into_string().unwrap();
+	let xcode_path = xcode_path.lines().next().unwrap();
     let sdks_path = Path::new(&xcode_path).join("Platforms/MacOSX.platform/Developer/SDKs");
     let last_sdk_entry = match fs::read_dir(&sdks_path){
         Ok(sdks) => sdks.last().unwrap().unwrap(),
         Err(_) => panic!("Couldn't find SDK at {}, probably xcode is not installed",sdks_path.to_str().unwrap())
     };
-    
+
     let sdk = last_sdk_entry.path().as_path().file_stem().unwrap().to_str().unwrap().to_string();
     if sdk.contains("MacOSX"){
         let version = &sdk[6..];
@@ -34,14 +32,13 @@ fn main() {
 	    let dest_path = Path::new(&out_dir).join("libfreeimage.a");
 	    fs::copy(freeimage_native_dir.join("Dist/libfreeimage.a"),dest_path).unwrap();
 	    println!("cargo:rustc-flags= -L native={}",out_dir);
-        
+
     }else{
         panic!("Couldn't find SDK at {}, probably xcode is not installed",sdks_path.to_str().unwrap())
     }
 }
 
-#[cfg(not(macos))]
-fn main() {
+fn build_linux() {
 	let freeimage_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 	let freeimage_native_dir = Path::new(&freeimage_dir).join("FreeImage");
     Command::new("make")
@@ -52,4 +49,31 @@ fn main() {
     let dest_path = Path::new(&out_dir).join("libfreeimage.a");
     fs::copy(freeimage_native_dir.join("Dist/libfreeimage.a"),dest_path).unwrap();
     println!("cargo:rustc-flags= -L native={}",out_dir);
+}
+
+fn build_emscripten() {
+	let freeimage_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+	let freeimage_native_dir = Path::new(&freeimage_dir).join("FreeImage");
+    Command::new("emmake")
+		.arg("make")
+	    .current_dir(&freeimage_native_dir)
+	    .arg("-j4")
+	    .status().unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("libfreeimage.a");
+    fs::copy(freeimage_native_dir.join("Dist/libfreeimage.a"),dest_path).unwrap();
+    println!("cargo:rustc-flags= -L native={}",out_dir);
+}
+
+fn main(){
+	let target_triple = env::var("TARGET").unwrap();
+	if target_triple.contains("linux") {
+		build_linux()
+	}else if target_triple.contains("macos") {
+		build_macos()
+	}else if target_triple.contains("emscripten") {
+		build_emscripten()
+	}else{
+		panic!("target OS {} not suported yet", target_triple);
+	}
 }
